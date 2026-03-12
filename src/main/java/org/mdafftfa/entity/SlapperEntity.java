26b5865670301e8e4263c794ff3fa359ef78dcaa
mdafftfa <=>
2026-03-12 15:16:24 +0700

    feat: - call SlapperHitEvent when attack entity - call SlapperDeletionEvent when attack entity - added commands ListTag and saved as NBT to receive list of commands in entity - added onUpdate for realtime saveNBT() so that the entity can't lost the data

--- src/main/java/org/mdafftfa/entity/SlapperEntity.java
+++ src/main/java/org/mdafftfa/entity/SlapperEntity.java
@@ -1,16 +1,33 @@
 package org.mdafftfa.entity;
 
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.command.ConsoleCommandSender;
 import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.data.EntityFlag;
+import cn.nukkit.event.entity.EntityDamageByEntityEvent;
 import cn.nukkit.event.entity.EntityDamageEvent;
 import cn.nukkit.level.format.IChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.nbt.tag.ListTag;
+
+import cn.nukkit.nbt.tag.StringTag;
 import org.jetbrains.annotations.NotNull;
+
+import org.mdafftfa.HitSessionType;
+import org.mdafftfa.Slapper;
 import org.mdafftfa.SlapperInterface;
+import org.mdafftfa.events.SlapperDeletionEvent;
+import org.mdafftfa.events.SlapperHitEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SlapperEntity extends Entity implements SlapperInterface {
 
-    public static String TAG = "Slapper Entity";
+    public static String TAG = "SlapperEntity";
+
+    private ListTag<StringTag> commands;
 
     public SlapperEntity(IChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
@@ -25,18 +42,25 @@ public class SlapperEntity extends Entity implements SlapperInterface {
         return Entity.AGENT;
     }
 
-    @Override
+    @Override @SuppressWarnings("unchecked")
     public void setDataFromEntity(Entity entity) {
-        SlapperEntity prevSlapper = (SlapperEntity) entity;
-        this.setNameTag(prevSlapper.getNameTag());
+        this.setNameTag(entity.namedTag.getString("NameTag"));
+        ListTag<StringTag> commandsTag = entity.namedTag.getList("Commands", StringTag.class);
+        this.commands.setAll(commandsTag.getAll());
     }
 
     @Override
     public void initEntity() {
         super.initEntity();
+        setCanBeSavedWithChunk(true);
 
-        this.namedTag.putBoolean(TAG, true);
-        this.namedTag.putString("SlapperType", getEntityType());
+        if (this.commands == null) {
+            if (namedTag.containsList("Commands")) {
+                this.commands = this.namedTag.getList("Commands", StringTag.class);
+            } else {
+                this.commands = new ListTag<>();
+            }
+        }
 
         this.setDataFlag(EntityFlag.CAN_SHOW_NAME, true);
         this.setDataFlag(EntityFlag.ALWAYS_SHOW_NAME, true);
@@ -44,27 +68,96 @@ public class SlapperEntity extends Entity implements SlapperInterface {
     }
 
     @Override
+    public void saveNBT() {
+        super.saveNBT();
+
+        this.namedTag.putBoolean(TAG, true);
+        this.namedTag.putString("SlapperType", getEntityType());
+        this.namedTag.putString("NameTag", this.getNameTag());
+        this.namedTag.putList("Commands", this.commands);
+    }
+
+    @Override
+    public boolean canBeSavedWithChunk() {
+        return true;
+    }
+
+    @Override
     public boolean onUpdate(int currentTick) {
-        // Untuk debugging
-//        if (currentTick % 100 == 0) { // Log setiap 100 tick
-//            Server.getInstance().getLogger().info(
-//                    "Slapper Entity - NameTag: '" + this.getNameTag() +
-//                            "', Visible: " + this.isNameTagAlwaysVisible() +
-//                            ", HasFlag: " + this.getDataFlag(EntityFlag.ALWAYS_SHOW_NAME)
-//            );
-//
-//            Server.getInstance().getLogger().info("Bool: "+ this.namedTag.getBoolean(TAG));
-//            Server.getInstance().getLogger().info("SlapperClass: "+ this.namedTag.getBoolean(TAG));
-//        }
+        saveNBT();
+        return isAlive();
+    }
 
+    @Override
+    public List<String> getCommands() {
+        List<String> listString = new ArrayList<>();
+        for (StringTag stringTag : commands.getAll()) {
+            listString.add(stringTag.data);
+        }
+        return listString;
+    }
 
-//        Server.getInstance().getLogger().info("oke");
-        return super.onUpdate(currentTick);
+    @Override
+    public void addCommand(String command) {
+        if (!this.commands.getAll().contains(new StringTag(command))) {
+            commands.add(new StringTag(command));
+        }
+    }
+
+    @Override
+    public boolean hasCommand(String command) {
+        return this.commands.getAll().contains(new StringTag(command));
+    }
+
+    @Override
+    public void removeCommand(String command) {
+        if (this.commands.getAll().contains(new StringTag(command))) {
+            this.commands.remove(new StringTag(command));
+        }
     }
 
     @Override
     public boolean attack(EntityDamageEvent source) {
-        this.close();
+        if (!(source instanceof EntityDamageByEntityEvent)) {
+            return false;
+        }
+
+        Entity damager = ((EntityDamageByEntityEvent) source).getDamager();
+        if (!(damager instanceof Player)) {
+            return false;
+        }
+
+        Player player = (Player) damager;
+        Entity entity = source.getEntity();
+        String prefix = Slapper.getInstance().prefix;
+
+        if (Slapper.getInstance().hitSessions.containsKey(player.getName())) {
+            int sessionType = Slapper.getInstance().hitSessions.get(player.getName());
+
+            if (sessionType == HitSessionType.ID) {
+                player.sendMessage(prefix + "Entity ID: " + entity.getId());
+                Slapper.getInstance().hitSessions.remove(player.getName());
+            } else if (sessionType == HitSessionType.REMOVE) {
+                SlapperDeletionEvent event = new SlapperDeletionEvent(entity, this.getEntityType(), player, SlapperDeletionEvent.CAUSE_INTERACT);
+                Server.getInstance().getPluginManager().callEvent(event);
+                if (!(event.isCancelled())) {
+                    player.sendMessage(prefix + "Entity removed!");
+                    entity.close();
+                    Slapper.getInstance().hitSessions.remove(player.getName());
+                }
+            }
+        } else {
+            SlapperHitEvent event = new SlapperHitEvent(entity, this.getEntityType(), player, SlapperHitEvent.CAUSE_INTERACT);
+            Server.getInstance().getPluginManager().callEvent(event);
+
+            if (!(event.isCancelled())) {
+                for (String command : getCommands()) {
+                    Server.getInstance().getCommandMap().executeCommand(new ConsoleCommandSender(), command.replace("{player}", "\"" + damager.getName() + "\""));
+                }
+            }
+        }
+
+        source.setCancelled();
         return super.attack(source);
     }
 }
